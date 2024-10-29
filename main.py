5a82dde89b7afa941febbd5c1d04124cebeda146
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-10-29 14:46:19 +0500

    fix(main): убраны лишние импорты

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-import os
-from log_strategy import FileLogStrategy, ConsoleLogStrategy, UpperFileLogStrategy
-from logger import Logger
 import datetime
-import threading
 import pathlib
 import os
+from app.logger import Logger
+from app.log_strategy import FileLogStrategy, ConsoleLogStrategy, UpperFileLogStrategy
 
 def main():
     save_folder_path = os.getcwd()
     log_file_name = f"DP.P1.{datetime.datetime.now().strftime('%Y-%m-%d.%H-%M-%S')}.log"
     log_file_path = pathlib.Path(save_folder_path) / log_file_name
 
-    logger = Logger()
-
-    # Логирую в файл в нижнем регистре
-    file_strategy = FileLogStrategy(log_file_path)
-    logger.trace("Начало работы программы", file_strategy)
-    logger.info("Программа запущена успешно", file_strategy)
+    # Логирую в файл
+    logger = Logger(FileLogStrategy(log_file_path))
+    logger.trace("Начало работы программы")
+    logger.info("Программа запущена успешно")
 
     # Логирование в консоль
-    console_strategy = ConsoleLogStrategy()
-    logger.warn("возможно проблемы", console_strategy)
-    logger.error("Ошибка ", console_strategy)
-    logger.fatal("Фатальная ошибка", console_strategy)
+    logger.set_writer(ConsoleLogStrategy())
+    logger.warn("возможно проблемы")
+    logger.error("Ошибка ")
+    logger.fatal("Фатальная ошибка")
 
     # Логирую в верхнем регистре
-    upper_file_strategy = UpperFileLogStrategy(log_file_path)
-    logger.info("Сообщение для верхнего регистра", upper_file_strategy)
-
+    logger.set_writer(UpperFileLogStrategy(log_file_path))
+    logger.info("Сообщение для верхнего регистра")
 
 if __name__ == "__main__":
     main()
